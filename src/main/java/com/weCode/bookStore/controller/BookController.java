9bebaa7fb9704997f7de3b519e5ca282e508012f
Vikas Kumar <your_email@abc.com>
2021-06-20 17:37:19 +0530

    book store api added

---
+++ src/main/java/com/weCode/bookStore/controller/BookController.java
@@ -0,0 +1,48 @@
+package com.weCode.bookStore.controller;
+
+import com.weCode.bookStore.dto.BookDto;
+import com.weCode.bookStore.model.Book;
+import com.weCode.bookStore.service.BookService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Api(value = "Book Api", tags = "Book Api", produces = "application/json")
+@RestController
+@RequestMapping("api/v1/books")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @ApiOperation(value = "get list of books", response = BookDto[].class, produces = "application/json")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list of book"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "not found resource")
+    })
+    @GetMapping
+    public ResponseEntity<List<BookDto>> getBooks() {
+        List<BookDto> books = bookService.getBooks();
+        return ResponseEntity.ok(books);
+    }
+
+    @GetMapping("/{title}")
+    public ResponseEntity<List<BookDto>> getBooksByTitle(@PathVariable("title") String title) {
+        List<BookDto> booksByTitle = bookService.getBooksByTitle(title);
+        return ResponseEntity.ok(booksByTitle);
+    }
+
+
+}
---
+++ src/main/java/com/weCode/bookStore/dto/BookDto.java
@@ -0,0 +1,55 @@
+package com.weCode.bookStore.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ApiModel(value = "BookDto", description = "All details about book")
+public class BookDto {
+
+    @ApiModelProperty(
+            readOnly = true,
+            value = "UUID",
+            dataType = "UUID",
+            example = "b5607d38-8fc1-43ef-b44e-34967083c80a",
+            notes = "The database generated uuid for book id",
+            required = true
+    )
+    private UUID id;
+
+    @ApiModelProperty(
+            value = "title",
+            dataType = "String",
+            example = "book title",
+            notes = "Book title",
+            required = true
+    )
+    private String title;
+
+    @ApiModelProperty(
+            value = "description",
+            dataType = "String",
+            example = "book description",
+            notes = "Book description",
+            required = true
+    )
+    private String description;
+
+    @ApiModelProperty(
+            value = "releaseYear",
+            dataType = "int",
+            example = "2020",
+            notes = "Book releaseYear",
+            required = true
+    )
+    private int releaseYear;
+}
